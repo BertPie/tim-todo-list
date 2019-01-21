d0b85e039ce19ce93d1102c09f9bc3e99d7c7059
Bert Pie <BertPieGIT@gmail.com>
2019-01-21 16:42:01 +0100

    add config and initializer

---
+++ src/main/java/pie/bert/config/WebConfig.java
@@ -0,0 +1,11 @@
+package pie.bert.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "pie.bert")
+public class WebConfig {
+}
